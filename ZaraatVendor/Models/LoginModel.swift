3d3668e0320f7b7a051a1644a8554dbf4c5dc317
Zafar Najmi <zafarnajmi.mytechnology@gmail.com>
2020-11-04 11:19:32 +0500

    Login Vendors "s" Issues Resolve

--- ZaraatVendor/Models/LoginModel.swift
+++ ZaraatVendor/Models/LoginModel.swift
@@ -22,7 +22,7 @@ struct LoginModel : Codable {
 
            case errors = "errors"
            case token = "token"
-           case vendors = "vendor"
+           case vendors = "vendors"
            case token_type = "token_type"
            case expires_in = "expires_in"
            case message = "message"
